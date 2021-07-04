d88211cf7c5556359d94d1ad94dcc24de57578e4
Adrian <Owinmowe@gmail.com>
2021-07-03 23:46:21 -0300

    Added full project to the github.

---
+++ UnityHDRP/Assets/AutoMoveCamera.cs
@@ -0,0 +1,32 @@
+﻿using Cinemachine;
+using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class AutoMoveCamera : MonoBehaviour
+{
+
+    [SerializeField] float speed = 1;
+
+    CinemachineTrackedDolly cmTrackedDolly = null;
+    Animator cameraLookAnimation = null;
+
+    private void Awake()
+    {
+        cameraLookAnimation = GetComponent<Animator>();
+        cameraLookAnimation.SetFloat("Speed", speed);
+        cmTrackedDolly = GetComponent<CinemachineVirtualCamera>().GetCinemachineComponent<CinemachineTrackedDolly>();
+    }
+
+    // Start is called before the first frame update
+    void Start()
+    {
+        
+    }
+
+    // Update is called once per frame
+    void Update()
+    {
+        cmTrackedDolly.m_PathPosition += Time.deltaTime * speed;
+    }
+}
